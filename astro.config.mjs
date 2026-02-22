01d0d5d03fbb703cd132d3d43541dee71af5c19e
Hasnat Jamil <hasnatjamil2018@gmail.com>
2026-02-23 04:42:51 +0600

    add placeholder pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,43 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "AppraiseJS",
+      customCss: ["./src/styles/custom.css"],
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/jamil2018/appraisejs-core",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Menu",
+          collapsed: false,
+          items: [
+            { slug: "menu/introduction" },
+            {
+              label: "GitHub",
+              link: "https://github.com/jamil2018/appraisejs-core",
+              attrs: {
+                target: "_blank",
+                rel: "noopener noreferrer",
+                class: "sidebar-link-github",
+              },
+            },
+          ],
+        },
+        {
+          label: "Getting Started",
+          collapsed: false,
+          autogenerate: { directory: "getting-started" },
+        },
+      ],
+    }),
+  ],
 });
